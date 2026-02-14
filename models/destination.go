e1cebcf866e8756962bd528b3153a0e95e79c6c7
Adixdix <94966565+Adixdix@users.noreply.github.com>
2026-02-14 23:15:41 +0100

    Format destination.go for consistency

--- models/destination.go
+++ models/destination.go
@@ -3,7 +3,8 @@ package models
 type Destination struct {
     ID          int    `json:"id"`
     Name        string `json:"name"`
-    Country     string `json:"country"`     // Ajouté pour correspondre à ta BDD
+    Country     string `json:"country"`     
     Description string `json:"description"`
-    Image       string `json:"image_url"`   // Correspond à image_url en BDD
+    Image       string `json:"image_url"`   
+
 }
